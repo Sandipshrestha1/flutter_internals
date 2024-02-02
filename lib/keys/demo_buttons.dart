0ee161b797425f93bddfc25dc01950ecfbcf4bdf
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-02-02 16:23:37 +0545

    buttons added

---
+++ lib/keys/demo_buttons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoButtons extends StatefulWidget {
+  const DemoButtons({super.key});
+
+  @override
+  State<DemoButtons> createState() => _DemoButtonsState();
+}
+
+class _DemoButtonsState extends State<DemoButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
